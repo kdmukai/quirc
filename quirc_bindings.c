ccec47579dcb2bc6914cf43e1c18cdd1e07110dc
kdmukai <kdmukai@gmail.com>
2022-11-02 17:41:15 -0500

    Interim commit with messy experiments in-progress

--- quirc_bindings.c
+++ quirc_bindings.c
@@ -3,6 +3,8 @@
 
 #include <string.h>
 
+// #include "esp_system.h"
+
 #include "py/obj.h"
 #include "py/runtime.h"
 #include "py/binary.h"
@@ -14,17 +16,17 @@
 /// package: foundation.qr
 
 #ifndef CONFIG_QUIRC_QR_MAX_HOR_RES
-#define CONFIG_QUIRC_QR_MAX_HOR_RES (240)
+#define CONFIG_QUIRC_QR_MAX_HOR_RES (120)
 #endif
 
 #ifndef CONFIG_QUIRC_QR_MAX_VER_RES
-#define CONFIG_QUIRC_QR_MAX_VER_RES (240)
+#define CONFIG_QUIRC_QR_MAX_VER_RES (120)
 #endif
 
 #define CONFIG_QUIRC_FLOOD_FILL_VARS (QUIRC_VARS_NUMOF(CONFIG_QUIRC_QR_MAX_VER_RES))
 
 #ifndef CONFIG_QR_DEBUG
-#define CONFIG_QR_DEBUG 0
+#define CONFIG_QR_DEBUG 1
 #endif
 
 #if CONFIG_QR_DEBUG
@@ -46,7 +48,12 @@ static quirc_flood_fill_vars_t _flood_fill_vars[CONFIG_QUIRC_FLOOD_FILL_VARS];
 static quirc_t                 _quirc;
 static quirc_code_t            _code;
 static quirc_data_t            _data;
-static uint8_t                 _framebuffer[CONFIG_QUIRC_QR_MAX_HOR_RES * CONFIG_QUIRC_QR_MAX_VER_RES];
+// static uint8_t                 _framebuffer[CONFIG_QUIRC_QR_MAX_HOR_RES * CONFIG_QUIRC_QR_MAX_VER_RES];
+
+// kdmukai: see: https://github.com/lvgl/lv_port_esp32/pull/259/commits/e80aa41467eab909d2203e31e7e73587958394f9
+// If we don't `malloc` here, compiler fails with "micropython.elf section `.dram0.bss` will not fit in region `dram0_0_seg`"
+// uint8_t*                _framebuffer = heap_caps_malloc(CONFIG_QUIRC_QR_MAX_HOR_RES * CONFIG_QUIRC_QR_MAX_VER_RES * sizeof(uint8_t), MALLOC_CAP_DMA);
+
 
 /// def init(hor_res: int, ver_res: int) -> None:
 ///     """
@@ -61,14 +68,23 @@ STATIC mp_obj_t mod_quirc_init(mp_obj_t hor_res_obj, mp_obj_t ver_res_obj) {
                           CONFIG_QUIRC_QR_MAX_HOR_RES, CONFIG_QUIRC_QR_MAX_VER_RES);
     }
 
-    if (quirc_init(&_quirc, hor_res, ver_res, _framebuffer, _flood_fill_vars, CONFIG_QUIRC_FLOOD_FILL_VARS) < 0) {
-        mp_raise_msg(&mp_type_RuntimeError, MP_ERROR_TEXT("Invalid buffer size for this decoder"));
-        return mp_const_none;
-    }
+    // uint8_t* _framebuffer = heap_caps_malloc(CONFIG_QUIRC_QR_MAX_HOR_RES * CONFIG_QUIRC_QR_MAX_VER_RES * sizeof(uint8_t), MALLOC_CAP_INTERNAL);
+
+    // if (quirc_init(&_quirc, hor_res, ver_res, _framebuffer, _flood_fill_vars, CONFIG_QUIRC_FLOOD_FILL_VARS) < 0) {
+    //     mp_raise_msg(&mp_type_RuntimeError, MP_ERROR_TEXT("Invalid buffer size for this decoder"));
+    //     return mp_const_none;
+    // }
+
+    quirc_new();
+    quirc_resize(&_quirc, hor_res_obj, ver_res_obj);
+
+    // Passing in a NULL framebuffer since we'll set it dynamically later with `set_image`
+    // quirc_init(&_quirc, hor_res, ver_res, NULL, _flood_fill_vars, CONFIG_QUIRC_FLOOD_FILL_VARS);
 
     return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_2(quirc_init_obj, mod_quirc_init);
+
 
 /// def scan() -> str:
 ///     '''
@@ -76,23 +92,26 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_2(quirc_init_obj, mod_quirc_init);
 ///
 ///     Returns a list of data found in the QR codes.
 ///     '''
-STATIC mp_obj_t mod_quirc_scan(void) {
+STATIC mp_obj_t mod_quirc_scan() {
     // Prepare to decode
     quirc_begin(&_quirc, NULL, NULL);
+
+    // DEBUG("%d\n", _framebuffer[(uint8_t)(60*CONFIG_QUIRC_QR_MAX_HOR_RES + 60)]);
+
     // This triggers the decoding of the image we just gave quirc
     quirc_end(&_quirc);
+    DEBUG("quirc_end complete\n");
 
     // Let's see if we got any results
     int num_codes = quirc_count(&_quirc);
-    DEBUG("num_codes=%d\n", num_codes);
     if (num_codes == 0) {
-        DEBUG("No codes found\n");
         return mp_const_none;
     }
 
+    DEBUG("num_codes=%d\n", num_codes);
+
     // Extract the first code found only, even if multiple were found
     quirc_extract(&_quirc, 0, &_code);
-    DEBUG("quirc_extract() done\n");
 
     // Decoding stage
     quirc_decode_error_t err = quirc_decode(&_code, &_data);
@@ -111,19 +130,47 @@ STATIC mp_obj_t mod_quirc_scan(void) {
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(quirc_scan_obj, mod_quirc_scan);
 
-STATIC mp_obj_array_t quirc_framebuffer_obj = {
-    .base     = {&mp_type_bytearray},
-    .typecode = BYTEARRAY_TYPECODE,
-    .free     = 0,
-    .len      = sizeof(_framebuffer),
-    .items    = _framebuffer,
-};
+
+/// def load_framebuffer(img_bytes) -> None:
+///     """
+///     Update the internal Quirc framebuffer to point to the specified pixel data.
+///     """
+STATIC mp_obj_t mod_quirc_load_framebuffer(size_t n_args, const mp_obj_t *args) {
+    mp_buffer_info_t pixel_data;
+    mp_get_buffer_raise(args[0], &pixel_data, MP_BUFFER_READ);
+
+    // memcpy(&_framebuffer, (uint8_t *)pixel_data.buf, sizeof(pixel_data.buf));
+    memcpy(_quirc.image, (uint8_t *)pixel_data.buf, _quirc.w * _quirc.h);
+    DEBUG("memcpy done\n");
+
+    // _quirc.image = (uint8_t *)pixel_data.buf;
+    // DEBUG("swapped internal image pointer\n");
+
+    // DEBUG("%d\n", _framebuffer[(60*CONFIG_QUIRC_QR_MAX_HOR_RES + 60)]);
+
+    mod_quirc_scan();
+
+    // DEBUG("%d\n", _framebuffer[(uint8_t)(60*CONFIG_QUIRC_QR_MAX_HOR_RES + 60)]);
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR(quirc_load_framebuffer_obj, (size_t)1, mod_quirc_load_framebuffer);
+
+
+// STATIC mp_obj_array_t quirc_framebuffer_obj = {
+//     .base     = {&mp_type_bytearray},
+//     .typecode = BYTEARRAY_TYPECODE,
+//     .free     = 0,
+//     .len      = sizeof(_framebuffer),
+//     .items    = _framebuffer,
+// };
 
 STATIC const mp_rom_map_elem_t quirc_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_quirc) },
     { MP_ROM_QSTR(MP_QSTR_init), MP_ROM_PTR(&quirc_init_obj) },
     { MP_ROM_QSTR(MP_QSTR_scan), MP_ROM_PTR(&quirc_scan_obj) },
-    { MP_ROM_QSTR(MP_QSTR_framebuffer), MP_ROM_PTR(&quirc_framebuffer_obj) }
+    { MP_ROM_QSTR(MP_QSTR_load_framebuffer), MP_ROM_PTR(&quirc_load_framebuffer_obj) },
+    // { MP_ROM_QSTR(MP_QSTR_framebuffer), MP_ROM_PTR(&quirc_framebuffer_obj) }
 };
 STATIC MP_DEFINE_CONST_DICT(quirc_module_globals, quirc_module_globals_table);
 
